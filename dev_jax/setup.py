a44f0fe044a28291ff55737d284d1962cde3112d
Ondrej Cifka <cifkao@users.noreply.github.com>
2020-12-29 14:11:35 +0100

    First attempt at porting scipy.signal to jax

---
+++ dev_jax/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Proceed to setup
+setup(
+    name='jax-spe',
+    version='0.1',
+    description='stochastic positional encoding for PyTorch',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['jax_spe'],
+    install_requires=[
+        'jax>=0.2.6'
+    ],
+)
