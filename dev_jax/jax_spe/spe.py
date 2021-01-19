2cf48defc11d4324c02cc214040c7dce86d58f60
Ondrej Cifka <cifkao@users.noreply.github.com>
2021-01-19 20:14:19 +0100

    Update JAX implementation

--- dev_jax/jax_spe/spe.py
+++ dev_jax/jax_spe/spe.py
@@ -4,88 +4,97 @@ from typing import Tuple, Union
 from flax import linen as nn
 import jax
 import jax.numpy as jnp
-from jax import lax
-from jax_spectral import stft, istft
 import numpy as np
 
 
-class SPE(nn.Module):
-    dimension: int = 1
-    resolution: Union[int, Tuple[int]] = 200
+class SineSPE(nn.Module):
+    num_heads: int = 1
+    in_features: Union[int, Tuple[int]] = 64
+    num_realizations: int = 64
+    num_sines: int = 10
+
+    """Sinusoidal stochastic positional encoding.
+
+    Args:
+        num_heads: The number of attention heads.
+        in_features: The number of input features per attention head.
+            If the actual key/query dimension is greater, only the
+            first `in_features` will be used and the rest will be
+            copied to the output unchanged. This is useful for keeping
+            some features non-positional.
+        num_realizations: The number of realizations of the stochastic
+            process (R).
+        num_sines: The number of sin and cos components (K).
+    """
 
     def setup(self):
-        if isinstance(self.resolution, int) or len(self.resolution.shape) == 0:
-            self.resolution = (self.resolution,) * self.dimension
+        params_shape = (self.num_heads, self.in_features, self.num_sines)
+        # bias initial frequencies to low values for long term range
+        self.freqs = self.param('freqs', lambda *args: jax.random.normal(*args) - 5.,
+                                params_shape)
+        self.offsets = self.param('offsets', jax.random.normal, params_shape)
+        self.gains = self.param('gains', jax.random.normal, params_shape)
 
-        self.msd = self.param(
-            'msd', smooth_init,
-            (self.resolution[0] + 1, *self.resolution[1:]))
-        self.window_size = [2 * d  for d in self.resolution]
-        self.hop_size = [d//4 for d in self.resolution]
+    def __call__(self, rng_key, length):
+        """
+        Generate sinusoidal SPEs.
+        """
+        # build omega_q and omega_k,
+        # with shape (num_heads, keys_dim, length, 2*num_sines)
+        indices = jnp.linspace(0, length-1, length)
 
-    def stft(self, x):
-        return stft(
-            x,
-            nperseg=self.window_size[0],
-            noverlap=self.window_size[0] - self.hop_size[0],
-            window='hamming',
-            #center=True,
-            #normalized=True,
-            return_onesided=True,
-            #pad_mode='reflect',
-            #return_complex=True
-        )[2]
+        # making sure the frequencies are in [0, 0.5]
+        freqs = jax.nn.sigmoid(self.freqs[:, :, None, :]) / 2.
 
-    def istft(self, x):
-        return istft(
-            x,
-            nperseg=self.window_size[0],
-            noverlap=self.window_size[0] - self.hop_size[0],
-            window='hamming',
-            #center=True,
-            #normalized=True,
-            input_onesided=True,
-            #length=shape[0]
-        )[1]
+        phases_q = (
+            2 * math.pi
+            * freqs * indices[None, None, :, None]
+            + self.offsets[:, :, None, :]
+        )
+        omega_q = jnp.stack([jnp.cos(phases_q), jnp.sin(phases_q)], axis=-1).reshape(
+            self.num_heads, self.in_features, length, 2*self.num_sines
+        )
 
-    def __call__(self, num: int, shape: Tuple[int]):
-        original_shape = shape
-        shape = [max(2*d, s) for (d,s) in zip(self.window_size, shape)]
-        print(shape)
-        if self.dimension != 1:
-            raise NotImplementedError("for now SPE only works in 1d")
+        phases_k = (
+            2 * math.pi
+            * freqs * indices[None, None, :, None]
+        )
+        omega_k = jnp.stack([jnp.cos(phases_k), jnp.sin(phases_k)], axis=-1).reshape(
+            self.num_heads, self.in_features, length, 2*self.num_sines
+        )
+
+        # gains is (num_heads, keys_dim, 2*num_sines). Making them nonnegative with softplus
+        gains = jax.nn.softplus(self.gains).repeat(2, axis=2)
 
         # draw noise of appropriate shape
-        # TODO: do not use the same key every time
-        p = jax.random.normal(jax.random.PRNGKey(0), (num, *shape))
-        eps = jnp.finfo(p.dtype).eps
+        z = jax.random.normal(
+            rng_key,
+            (1, self.num_heads, self.in_features, 2 * self.num_sines, self.num_realizations),
+        ) / math.sqrt(self.num_realizations * self.in_features)
 
-        msd = nn.relu(self.msd)
-        msd = msd / (jnp.linalg.norm(msd) + eps)
+        # scale each of the 2*num_sines by the appropriate gain
+        # z is still (1, num_heads, keys_dim, 2*num_sines, num_realizations)
+        z = z * gains[None, ..., None]
 
-        # compute its STFT
-        p = self.stft(p)
+        # computing the sum over the sines.
+        # gets (1, num_heads, keys_dim, length, num_realizations)
+        qbar = jnp.matmul(omega_q[None], z)
+        kbar = jnp.matmul(omega_k[None], z)
 
-        # filter by the magnitude spectral density
-        p = p * msd[None, :, None]
+        # permuting them to be (1, length, num_heads, keys_dim, num_realizations)
+        qbar = jnp.transpose(qbar, (0, 3, 1, 2, 4))
+        kbar = jnp.transpose(kbar, (0, 3, 1, 2, 4))
 
-        # compute istft
-        p = self.istft(p)  # shape=shape
-
-        # weight by window in case of non perfect reconstruction
-        #weight = torch.ones(*shape, device=p.device)
-        #p = p / self.istft(self.stft(weight), shape=shape)[None]
-
-        #normalize to get correlations
-        p = p / (jnp.linalg.norm(p, axis=0) + eps)
-
-        # truncate if needed
-        p = p[tuple(slice(s) for s in (num, ) + original_shape)]
-        return p
+        return qbar, kbar
 
 
-def smooth_init(key, shape, dtype=jnp.float32):
-    msd = np.zeros(shape) + 1e-3
-    L = min(msd.shape[0], 10)
-    msd[:L] = msd[:L] + np.logspace(0, -2, L)
-    return jnp.asarray(msd)
+def apply_spe(keys, spe):
+    # split off the non-positional part
+    keys, keys_rest = jnp.split(keys, [spe.shape[-2]], axis=-1)
+
+    # sum over the keys_dim after multiplying by queries and keys
+    # spe is (1, max_len, ...), truncating and broadcasting over the batch
+    keys = (spe[:, :keys.shape[1]] * keys[..., None]).sum(axis=-2)
+
+    # add the non-positional part back
+    return jnp.concatenate([keys, keys_rest], axis=-1)
