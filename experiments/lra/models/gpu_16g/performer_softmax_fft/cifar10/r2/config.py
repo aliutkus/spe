9e95832782df7975fb39916fe7cf8acabbecbd7b
maximzubkov <zubkov.md@phystech.edu>
2021-08-30 13:14:57 +0300

    Fix fft type in performer cifar

--- experiments/lra/models/gpu_16g/performer_softmax_fft/cifar10/r2/config.py
+++ experiments/lra/models/gpu_16g/performer_softmax_fft/cifar10/r2/config.py
@@ -51,7 +51,7 @@ def get_config():
     lax_scan_unroll=16)
   config.model_kwargs = dict(
     pos_bias_cfg=dict(
-      pos_bias_type="fft",
+      pos_bias_type="fft_2d",
       bias_base_type="full",
       lm=False,
       has_bos=False,
