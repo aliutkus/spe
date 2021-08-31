c94a4292de14d127a792057032b9c6c6532bd47e
maximzubkov <zubkov.md@phystech.edu>
2021-08-31 23:53:51 +0300

    Original transformer configs

---
+++ experiments/lra/models/gpu_16g/transformer/listops/r3/config.py
@@ -0,0 +1,39 @@
+# Copyright 2020 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Configuration and hyperparameter sweeps."""
+
+from fast_self_attention import fast_self_attention as favor
+import jax
+
+from lra_benchmarks.listops.configs import base_listops_config
+
+def dpfp(x, nu=1):
+  x = jax.numpy.concatenate([jax.nn.relu(x), jax.nn.relu(-x)], axis=-1)
+  x_rolled = jax.numpy.concatenate([jax.numpy.roll(x, shift=j, axis=-1) for j in range(1, nu + 1)], axis=-1)
+  x_repeat = jax.numpy.concatenate([x] * nu, axis=-1)
+  return x_repeat * x_rolled
+
+def get_config():
+  """Get the default hyperparameter configuration."""
+  config = base_listops_config.get_config()
+  config.random_seed = 2
+  config.model_type = "transformer"
+  config.batch_size = 8
+  config.learning_rate = config.learning_rate / 32 * 8
+  config.num_train_steps = 10000
+  return config
+
+
+def get_hyper(hyper):
+  return hyper.product([])
